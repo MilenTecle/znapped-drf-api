c64de1f9a25ed423845af49e332520c4fd1195ee
Milen Tecle <m_tecle@hotmail.com>
2024-10-12 19:07:08 +0000

    comment_count and likes_count fields added to PostList and Profiles

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,14 +7,16 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(
-        source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
-             raise serializers.ValidationError(
+            raise serializers.ValidationError(
                 'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
@@ -39,6 +41,8 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_at', 'updated_at', 'title', 'content', 'image'
-            ]
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter',
+            'like_id', 'likes_count', 'comments_count',
+        ]
