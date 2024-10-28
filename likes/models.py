92fc181f10945e25b819e57d57d75da3f8c94f34
Milen Tecle <m_tecle@hotmail.com>
2024-10-28 20:08:41 +0000

    update like model to include reactions

--- likes/models.py
+++ likes/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
+class Reaction(models.TextChoices):
+  HEART = 'heart', 'Heart'
+  THUMBS_UP = 'thumps_up', 'Thumps Up'
+  LAUGH = 'laugh', 'Laugh'
+  SAD = 'sad', 'Sad'
+  ANGRY = 'angry', 'Angry'
 
 class Like(models.Model):
     """
@@ -14,12 +20,15 @@ class Like(models.Model):
         Post, related_name='likes', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    reaction_type = models.CharField(
+        max_length=10, choices=Reaction.choices, default=Reaction.HEART
+    )
 
     class Meta:
         ordering = ['-created_at']
-        unique_together = ['owner', 'post']
+        unique_together = ['owner', 'post', 'reaction_type']
 
     def __str__(self):
-        return f'{self.owner} {self.post}'
+        return f'{self.owner} {self.post} {self.reaction_type}'
 
 
