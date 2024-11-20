d3a2130fe93ec3a02586f67bade5509a1c420fca
Milen Tecle <m_tecle@hotmail.com>
2024-11-20 20:49:37 +0000

    add JWT_AUTH_HTTPONLY = True to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,6 +48,7 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+JWT_AUTH_HTTPONLY = True
 
 if (
     'ACCESS_TOKEN_LIFETIME' in os.environ and
