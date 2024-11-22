719f6efb0f85d14d980025e61fb8b323ba7484dd
Milen Tecle <m_tecle@hotmail.com>
2024-11-22 21:01:25 +0000

    update DirectMessageSerializer to include sender_name and receiver_name

--- direct_messages/serializers.py
+++ direct_messages/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import DirectMessage
 
 class DirectMessageSerializer(serializers.ModelSerializer):
-  sender = serializers.ReadOnlyField(source='sender.username')
-  receiver = serializers.ReadOnlyField(source='receiver.username')
+  sender_name = serializers.ReadOnlyField(source='sender.username')
+  receiver_name = serializers.ReadOnlyField(source='receiver.username')
 
   class Meta:
     model = DirectMessage
-    fields = ['id', 'sender', 'receiver', 'content', 'created_at', 'read']
+    fields = ['id', 'sender', 'receiver', 'sender_name', 'receiver_name',
+    'content', 'created_at', 'read'
+  ]
