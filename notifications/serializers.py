2a3e59e8a07753f65ace1e61913c1d11ba0cced1
Milen Tecle <m_tecle@hotmail.com>
2024-11-24 20:56:16 +0000

    add sender_profile_id to notificationserializer to link to the senders profile

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,11 +4,12 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
   user = serializers.ReadOnlyField(source='user.username')
   sender = serializers.ReadOnlyField(source='sender.username')
+  sender_profile_id = serializers.ReadOnlyField(source='sender.profile.id')
   post_id = serializers.ReadOnlyField(source='post_id.id')
   direct_message_id = serializers.ReadOnlyField(source='direct_message_id.id')
 
   class Meta:
     model = Notification
     fields = ['id', 'user', 'sender', 'type', 'message', 'created_at', 'read',
-    'post_id', 'direct_message_id'
+    'post_id', 'direct_message_id', 'sender_profile_id'
   ]
